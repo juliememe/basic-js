d79914ee2b4c5a271d87dc5db29e01f67360b3f5
julie_meme <julie_meme@mail.ru>
2020-10-02 11:38:01 +0400

    feat: add solution

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function countCats(backyard) {
+  let counter = 0;
+  for (const cat of backyard) {
+    for(let everyCat of cat){
+    if (everyCat === "^^") {
+      counter++;
+    }
+  }
+}
+  if (counter === 0) {
+    return 0;
+  } else {
+    return counter;
+  }
+}
