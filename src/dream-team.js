6f2feac34ff263c64aa9571e0fa0b7bd22e781a2
julie_meme <julie_meme@mail.ru>
2020-10-06 13:46:58 +0400

    feat: add solution to the dream-team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members) || members === null) {
+    return false;
+  } else {
+    
+    let list = [];
+    for (const names of members) {
+      if (typeof names == "string") {
+        list.push(names.toUpperCase().trim());
+      }
+    }
+    return list
+      .sort()
+      .reduce((acc, currentValue) => acc + currentValue[0], "");
+  }
 };
