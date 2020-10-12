9c396cf7513efb23deb4af93df8a464536592f74
julie_meme <julie_meme@mail.ru>
2020-10-12 15:25:14 +0400

    feat: add solution to extended-repeator

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,30 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
-  
+module.exports = 
+function repeater(str, options) {
+  let result = "";
+  let [
+    repeatTimes = 1,
+    separator = "+",
+    addition = "",
+    additionRepeatTimes = 1,
+    additionSeparator = "|",
+  ] = Object.values(options);
+
+  for (let i = 0; i < repeatTimes; i++) {
+    result += String(str);
+
+    for (let j = additionRepeatTimes; j > 0; j--) {
+      if (j > 1) {
+        result = result + String(addition) + additionSeparator;
+      } else if (j === 1) {
+        result = result + String(addition);
+      } else continue;
+    }
+
+    if (i < repeatTimes-1) {
+      result += separator;
+    }
+  }
+  return result;
+}
