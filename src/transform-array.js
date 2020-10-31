7b5e0e0ca9db2a8db96cc9da4c33c80259f8e088
julie_meme <julie_meme@mail.ru>
2020-11-01 01:20:01 +0400

    feat: add solution transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,84 +1,30 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform(array) {
-  let newArr = [];
-  if (Array.isArray(array)) {
-    for (let arr of array) {
-      if (Array.isArray(arr)) {
-        for (let i = 0; i < arr.length; i++) {
-          newArr.push(arr[i]);
-          if (arr[i] === "--discard-next") {
-            if (i != arr.length - 1) {
-              i += 2;
-              newArr.pop(arr[i]);
-            } else {
-              newArr.pop(arr[i]);
-            }
-          } else if (arr[i] === "--discard-prev" ){
-              if(i !== 0) {
-                newArr.pop(arr[i - 1]);
-                newArr.pop(arr[i])
-          } else{
-            newArr.pop(arr[i])
-          } 
-           } else if (arr[i] === "--double-next"){
-              if(i != arr.length - 1) {
-                newArr.pop(arr[i]);
-                newArr.push(arr[i + 1]);
-          } else{
-            newArr.pop(arr[i]);
-          }
-          } else if (arr[i] === "--double-prev"){
-              if(i !== 0) {
-            newArr.pop(arr[i]);
-            newArr.push(arr[i - 1]);
-          } else{
-            newArr.pop(arr[i]);
-          }
-              
+  if (Array.isArray(array)){
+    let newArray =[];
+    for (let i = 0; i < array.length; i++) {
+        if(array[i] !=='--discard-next' && array[i] !=='--discard-prev' && array[i] !=="--double-next" && array[i]!=="--double-prev" ){
+          newArray.push(array[i]);
         }
-        else continue;
-
-      } 
-    }else {
-        for (let i = 0; i < array.length; i++) {
-            newArr.push(array[i]);
-            if (array[i] === "--discard-next") {
-              if (i != array.length - 1) {
-                i += 2;
-                newArr.pop(array[i]);
-              } else {
-                newArr.pop(array[i]);
-              }
-            } else if (array[i] === "--discard-prev" ){
-                if(i !== 0) {
-                  newArr.pop(array[i - 1]);
-                  newArr.pop(array[i])
-            } else{
-              newArr.pop(array[i])
-            } 
-             } else if (array[i] === "--double-next"){
-                if(i != array.length - 1) {
-                  newArr.pop(array[i]);
-                  newArr.push(array[i + 1]);
-            } else{
-              newArr.pop(array[i]);
-            }
-            } else if (array[i] === "--double-prev"){
-                if(i !== 0) {
-              newArr.pop(array[i]);
-              newArr.push(array[i - 1]);
-            } else{
-              newArr.pop(array[i]);
-            }
-           }
-          else continue;
+       else if(array[i] === '--discard-next' && i !== array.length-1){
+          i++;
         }
-        return newArr;
-      }
+        else if(array[i] === '--discard-prev' && array[i - 2] !== '--discard-next'&& i !==0){
+          newArray.pop();
+        }
+       else if(array[i] === '--double-next' && i !== array.length-1){
+          newArray.push(array[i+1]);
+         
+        }
+      else if(array[i] ==='--double-prev' && array[i - 2] !== '--discard-next' && i!== 0 ){
+          newArray.push(array[i-1]);
+        }
+      
     }
-    return newArr;
-  } else {
-    throw new Error("Not an Array");
+    return newArray;
   }
-}
+  
+    throw Error;
+  
+};
